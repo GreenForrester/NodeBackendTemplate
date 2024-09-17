1d814c4aca575c38d6ad0ad2167b90ddd1f6e5f7
GreenForests <bsaadi.mail@gmail.com>
2024-09-17 20:36:07 +0500

    Build

--- routes/users.ts
+++ routes/users.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req: Request, res: Response, next: NextFunction) {
-  res.send('respond with a resource');
+  res.send('respond with a resource AAA');
 });
 
 export default router;
